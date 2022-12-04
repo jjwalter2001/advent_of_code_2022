0bf31296ff4b73efbb5488ace79a32641775ec82
Joe Walter <jjwalter2001@gmail.com>
2022-12-04 11:14:26 -0500

    day 4

---
+++ day04/day04-part1.py
@@ -0,0 +1,18 @@
+import sys
+import io
+
+numoverlaps = 0
+
+with open('day04\\input.txt') as f:
+    for line in f:
+        (grp1, grp2) = line.strip().split(',')
+        (rng1_1, rng1_2) = grp1.split('-')
+        (rng2_1, rng2_2) = grp2.split('-')
+
+        set1 = set(range(int(rng1_1), int(rng1_2) + 1))
+        set2 = set(range(int(rng2_1), int(rng2_2) + 1))
+
+        if set1.issubset(set2) or set2.issubset(set1):
+            numoverlaps += 1
+
+print(f'numoverlaps: {numoverlaps}')
