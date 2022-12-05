f2474c6dae04f3ab3787bc76e405db0ff57dbd95
Joe Walter <jjwalter2001@gmail.com>
2022-12-05 08:21:47 -0500

    day5

---
+++ day05/day05-part2.py
@@ -0,0 +1,58 @@
+import sys
+import io
+
+# This dictionary will be keyed on the stack number
+# and contain a list which operates as a stack (i.e. push/pop)
+stacks = {}
+# This lookup table holds the column number of each stack in the input
+pos = {1: 1, 2: 5, 3: 9, 4: 13, 5: 17, 6: 21, 7: 25, 8: 29, 9: 33}
+blank = False
+
+
+def move_crates(mvmt):
+    # Split the move command by spaces and then grab the relevant components
+    flds = mvmt.strip().split(' ')
+    num_crates = int(flds[1])
+    from_st = int(flds[3])
+    to_st = int(flds[5])
+
+    # For part two, we move the whole range of creates at one time
+    # Grab the last x items from the list and move it to the other list
+    stacks[to_st] += stacks[from_st][len(stacks[from_st]) - num_crates:]
+    # Then remove them from the source list
+    stacks[from_st] = stacks[from_st][:-num_crates]
+
+    return None
+
+with open('day05\\input.txt') as f:
+    for line in f:
+        if line == '\n':
+            continue
+
+        # The current stack definition is until we get to the blank line
+        if line.startswith(' 1'):
+            blank = True
+
+            # Once we get here, we're done building the stacks, so
+            # just need to reverse them to get them into proper order
+            for st in stacks.keys():
+                stacks[st].reverse()
+
+            continue
+
+        if not blank:
+            # We can assume clean input and each stack is in specific columns...
+            # If the stack has a value there, then add it to our list
+            for st in pos.keys():
+                if (line[pos[st]] != ' '):
+                    if not st in stacks:
+                        stacks[st] = list()
+                    stacks[st].append(line[pos[st]])
+
+        else:
+            # Process a move command
+            move_crates(line)
+
+
+for x in range(1, 10):
+    print(f'stack {x}: {" " if len(stacks[x])==0 else stacks[x].pop()}')
